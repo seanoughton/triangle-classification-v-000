aff5b0f674b69d83e94c1f5f33b39aa30cb686e2
Sean Oughton <seanoughton@gmail.com>
2018-02-13 19:05:19 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,14 +8,15 @@ class Triangle
   def kind()
     #Further, each side must be larger than 0.
     #The sum of the lengths of any two sides of a triangle always exceeds the length of the third side. This is a principle known as the triangle inequality.
-
-    if (@side_1 == @side_2) && (@side_2 == @side_3)
-      :equilateral
-    elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
-      :isosceles
-    elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
-      :scalene
+    if (#some condition for a false triangle)
     end
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      end
 
   end
 
