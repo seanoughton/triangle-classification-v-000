2540b90d2e624ac4ac08ea027177ab04aae4eaca
Sean Oughton <seanoughton@gmail.com>
2018-02-13 19:10:10 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
     #The sum of the lengths of any two sides of a triangle always exceeds the length of the third side. This is a principle known as the triangle inequality.
     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
       raise TriangleError
-    elsif (@side_1+@side_2 < @side_3)
+    elsif (@side_1+@side_2 < @side_3) || (@side_1+@side_3 < @side_2) || (@side_2+@side_3 < @side_1)
       raise TriangleError
     else
       if (@side_1 == @side_2) && (@side_2 == @side_3)
