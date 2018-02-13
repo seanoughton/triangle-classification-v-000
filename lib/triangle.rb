afe015c4fffc76c10d35ec93484c3e021c98c35c
Sean Oughton <seanoughton@gmail.com>
2018-02-13 18:54:11 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+  def initialize(side_1,side_2,side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind()
+    #Further, each side must be larger than 0.
+    #The sum of the lengths of any two sides of a triangle always exceeds the length of the third side. This is a principle known as the triangle inequality.
+    if (self.side_1 == self.side_2) && (self.side_2 == self.side_3) && (self.side1 == self.side_3)
+      :equilateral
+    end
+    #An isosceles triangle is a triangle that has two equal sides.
+    #An equilateral triangle is a triangle that has three equal sides
+    #Scalene triangle: A scalene triangle is a triangle that has no equal sides.
+    #returns, as a symbol, its type :equilateral, :isosceles, :scalene
+  end
+
+end
+
+class TriangleError < StandardError
+  # triangle error code
 end
