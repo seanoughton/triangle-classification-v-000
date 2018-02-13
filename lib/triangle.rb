aec1e66873f93e2bb70b490c9a8d4c7eff5c2828
Sean Oughton <seanoughton@gmail.com>
2018-02-13 18:57:28 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
       :equilateral
     end
     #An isosceles triangle is a triangle that has two equal sides.
-    if (@side_1 == @side_2) || (@side_2 == @side_3)
+    if (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
       :isosceles
     end
 
