16a9181502a907cc57083a9757ebf5b0fdee2ca8
Sean Oughton <seanoughton@gmail.com>
2018-02-13 18:59:14 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
       :equilateral
     elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
       :isosceles
-    elsif condition
+    elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
       :scalene
     end
     #An isosceles triangle is a triangle that has two equal sides.
