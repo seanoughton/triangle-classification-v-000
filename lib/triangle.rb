a51e36851c27e9dc7d6cd0e232dd68436330efad
Sean Oughton <seanoughton@gmail.com>
2018-02-13 19:05:34 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,7 @@ class Triangle
     #Further, each side must be larger than 0.
     #The sum of the lengths of any two sides of a triangle always exceeds the length of the third side. This is a principle known as the triangle inequality.
     if (#some condition for a false triangle)
+      raise TriangleError
     end
       if (@side_1 == @side_2) && (@side_2 == @side_3)
         :equilateral
